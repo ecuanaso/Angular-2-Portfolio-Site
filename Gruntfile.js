fcc63f4097a93867a78b78a4fd6313509fff378d
David Castillo <miestilo23@gmail.com>
2017-01-11 14:32:28 -0500

    Working on Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,17 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+	grunt.initConfig({
+	  uglify: {
+	    build: {
+	        src: 'app/main.js',
+	        dest: 'build/js/scripts.js'
+        }
+	  }
+	});
 
-  // // Load the plugin that provides the "uglify" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // // Default task(s).
-  // grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('build', ['uglify'] );
 
 };
